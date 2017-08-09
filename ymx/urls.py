fee4a2db17397c5ef0a2115e2995db5eef7629bd
Peng Wang <randywang001@gmail.com>
2017-08-08 22:06:59 -0700

    1. Add index.html page

--- ymx/urls.py
+++ ymx/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'ymx'  # this is the namespace
 
 urlpatterns = [
-    # /music/
-    url(r'^$', views.index, name='index'),
+    # /ymx/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[-\w]+)/$', views.IndexView.as_view(), name='index'),
 
 ]
--- ymx/views.py
+++ ymx/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.views import generic
-from django.http import HttpResponse
+from .models import company
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, 张译文")
+class IndexView(generic.DetailView):
+    template_name = 'ymx/index.html'
+    context_object_name = 'CompanyDetail'
+    model = company
