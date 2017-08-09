542aff7277a963c78f17d40501525833d6bcee99
Peng Wang <randywang001@gmail.com>
2017-08-08 21:20:26 -0700

    1 激活admin 2 建立company model

--- ymx/models.py
+++ ymx/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class company(models.Model):
+    company_name = models.CharField(max_length=500)
+    company_description = models.CharField(max_length=5000, blank=True)
+    company_primary_phone = models.CharField(max_length=100, blank=True)
+    company_secondary_phone = models.CharField(max_length=100, blank=True)
+    company_email = models.CharField(max_length=100, default='xxx@email.com', blank=True)
+    company_address = models.CharField(max_length=500, blank=True)
+    company_logo = models.FileField(blank=True)
+
+    def __str__(self):
+        return 'Company - ' + self.company_name
